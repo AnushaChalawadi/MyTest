c00170ff600a02fce82133f6133f8b1496021edb
Anusha C <anusha.c@goavega.com>
2019-03-17 16:16:49 +0530

    Set-up template for selenium Automation

---
+++ Auto_Const.java
@@ -0,0 +1,19 @@
+package generic;
+
+public interface Auto_const 
+{
+	//fire fox browser
+	
+	String GECKO_KEY="webdriver.gecko.driver";
+	String GECKO_VALUE="./sofware/geckodriver.exe";
+	
+	// chrome browser
+	
+	//String CHROME_KEY="webdriver.chrome.driver";
+	//String CHROME_VALUE="./sofware/chromedriver.exe";
+	
+	//Excel file path
+	
+	//String PATH="./Excel/demo.xlsx";
+	
+}
